0ae1c131eb1d35773f32944ad5c52c18bb6b9ec6
hakalju <juha.hakala@logisnext.eu>
2021-08-15 02:25:03 +0300

    Teams started. Next command to add teammate and change drivers

--- projectWpf/Sources/pages/MemObj.cs
+++ projectWpf/Sources/pages/MemObj.cs
@@ -16,21 +16,18 @@ namespace projectWpf.Sources.pages
 	};
 	class ParticipantInfo : BaseViewModel
 	{
-		//static int STRING_LENGTH_MAX = 64;
-
 		bool _mIsActive;
 		string _mName;
-		ObservableCollection<float> _mWorldPosition;// change to ObservableCollection<float> and init in constructor
+		ObservableCollection<float> _mWorldPosition;
 		float _mCurrentLapDistance;
 		uint _mRacePosition;
 		uint _mLapsCompleted;
 		uint _mCurrentLap;
 		int _mCurrentSector;
-		//100bytes
+
 		float _mCurrentSector1Time;
 		float _mCurrentSector2Time;
 		float _mCurrentSector3Time;
-
 		float _mFastestSector1Time;
 		float _mFastestSector2Time;
 		float _mFastestSector3Time;
@@ -42,34 +39,245 @@ namespace projectWpf.Sources.pages
 		uint _mPitMode;
 		float _mSpeed;
 
-
-		public bool mIsActive { get { return _mIsActive; } set { _mIsActive = value; NotifyPropertyChanged("mIsActive"); } }
-		public string mName { get { return _mName; } set { _mName = value; NotifyPropertyChanged("mName"); } }
-		public ObservableCollection<float> mWorldPosition { get { return _mWorldPosition; } set { _mWorldPosition = value; NotifyPropertyChanged("mWorldPosition"); } }
-		public float mCurrentLapDistance { get { return _mCurrentLapDistance; } set { _mCurrentLapDistance = value; NotifyPropertyChanged("mCurrentLapDistance"); } }
-		public uint mRacePosition { get { return _mRacePosition; } set { _mRacePosition = value; NotifyPropertyChanged("mRacePosition"); } }
-		public uint mLapsCompleted { get { return _mLapsCompleted; } set { _mLapsCompleted = value; NotifyPropertyChanged("mLapsCompleted"); } }
-		public uint mCurrentLap { get { return _mCurrentLap; } set { _mCurrentLap = value; NotifyPropertyChanged("mCurrentLap"); } }
-		public int mCurrentSector { get { return _mCurrentSector; } set { _mCurrentSector = value; NotifyPropertyChanged("mCurrentSector"); } }
-
-		public float mCurrentSector1Time { get { return _mCurrentSector1Time; } set { _mCurrentSector1Time = value; NotifyPropertyChanged("mCurrentSector1Time"); } }
-		public float mCurrentSector2Time { get { return _mCurrentSector2Time; } set { _mCurrentSector2Time = value; NotifyPropertyChanged("mCurrentSector2Time"); } }
-		public float mCurrentSector3Time { get { return _mCurrentSector3Time; } set { _mCurrentSector3Time = value; NotifyPropertyChanged("mCurrentSector3Time"); } }
-		public float mFastestSector1Time { get { return _mFastestSector1Time; } set { _mFastestSector1Time = value; NotifyPropertyChanged("mFastestSector1Time"); } }
-		public float mFastestSector2Time { get { return _mFastestSector2Time; } set { _mFastestSector2Time = value; NotifyPropertyChanged("mFastestSector2Time"); } }
-		public float mFastestSector3Time { get { return _mFastestSector3Time; } set { _mFastestSector3Time = value; NotifyPropertyChanged("mFastestSector3Time"); } }
-		public float mFastestLapTime { get { return _mFastestLapTime; } set { _mFastestLapTime = value; NotifyPropertyChanged("mFastestLapTime"); } }
-		public float mLastLapTime { get { return _mLastLapTime; } set { _mLastLapTime = value; NotifyPropertyChanged("mLastLapTime"); } }
-		public bool mLapInvalidated { get { return _mLapInvalidated; } set { _mLapInvalidated = value; NotifyPropertyChanged("mLapInvalidated"); } }
-		public uint mRaceState { get { return _mRaceState; } set { _mRaceState = value; NotifyPropertyChanged("mRaceState"); } }
-		public uint mPitMode { get { return _mPitMode; } set { _mPitMode = value; NotifyPropertyChanged("mPitMode"); } }
-		public float mSpeed { get { return _mSpeed; } set { _mSpeed = value; NotifyPropertyChanged("mSpeed"); } }
-
-
-
-
 		private string _cColor;
-		public string cColor { get { return _cColor; } set { _cColor = value; NotifyPropertyChanged("cColor"); } }
+		private CTeam _cTeam;
+
+		public bool mIsActive {
+			get { return _mIsActive; }
+			set
+			{
+				if (_mIsActive != value)
+				{
+					_mIsActive = value;
+					NotifyPropertyChanged("mIsActive");
+				}
+			}
+		}
+		public string mName {
+			get { return _mName; }
+			set
+			{
+				if (_mName != value)
+				{
+					_mName = value;
+					NotifyPropertyChanged("mName");
+				}
+			}
+		}
+		public ObservableCollection<float> mWorldPosition {
+			get { return _mWorldPosition; }
+			set
+			{
+				_mWorldPosition = value;
+				NotifyPropertyChanged("mWorldPosition");
+			}
+		}
+		public float mCurrentLapDistance {
+			get { return _mCurrentLapDistance; }
+			set
+			{
+				_mCurrentLapDistance = value;
+				NotifyPropertyChanged("mCurrentLapDistance");
+			}
+		}
+		public uint mRacePosition {
+			get { return _mRacePosition; }
+			set
+			{
+				if (_mRacePosition != value)
+				{
+					_mRacePosition = value;
+					NotifyPropertyChanged("mRacePosition");
+				}
+			}
+		}
+		public uint mLapsCompleted {
+			get { return _mLapsCompleted; }
+			set
+			{
+				if (_mLapsCompleted != value)
+				{
+					_mLapsCompleted = value;
+					NotifyPropertyChanged("mLapsCompleted");
+				}
+			}
+		}
+		public uint mCurrentLap
+		{
+			get { return _mCurrentLap; }
+			set
+			{
+				if (_mCurrentLap != value)
+				{
+					_mCurrentLap = value;
+					NotifyPropertyChanged("mCurrentLap");
+				}
+			}
+		}
+		public int mCurrentSector
+		{
+			get { return _mCurrentSector; }
+			set
+			{
+				if (_mCurrentSector != value)
+				{
+					_mCurrentSector = value;
+					NotifyPropertyChanged("mCurrentSector");
+				}
+			}
+		}
+		public float mCurrentSector1Time {
+			get { return _mCurrentSector1Time; }
+			set
+			{
+				if (_mCurrentSector1Time != value)
+				{
+					_mCurrentSector1Time = value;
+					NotifyPropertyChanged("mCurrentSector1Time");
+				}
+			}
+		}
+		public float mCurrentSector2Time {
+			get { return _mCurrentSector2Time; }
+			set
+			{
+				if (_mCurrentSector2Time != value)
+				{
+					_mCurrentSector2Time = value;
+					NotifyPropertyChanged("mCurrentSector2Time");
+				}
+			}
+		}
+		public float mCurrentSector3Time {
+			get { return _mCurrentSector3Time; }
+			set
+			{
+				if (_mCurrentSector3Time != value)
+				{
+					_mCurrentSector3Time = value;
+					NotifyPropertyChanged("mCurrentSector3Time");
+				}
+			}
+		}
+		public float mFastestSector1Time {
+			get { return _mFastestSector1Time; }
+			set
+			{
+				if (_mFastestSector1Time != value)
+				{
+					_mFastestSector1Time = value;
+					NotifyPropertyChanged("mFastestSector1Time");
+				}
+			}
+		}
+		public float mFastestSector2Time {
+			get { return _mFastestSector2Time; }
+			set
+			{
+				if (_mFastestSector2Time != value)
+				{
+					_mFastestSector2Time = value;
+					NotifyPropertyChanged("mFastestSector2Time");
+				}
+			}
+		}
+		public float mFastestSector3Time {
+			get { return _mFastestSector3Time; }
+			set
+			{
+				if (_mFastestSector3Time != value)
+				{
+					_mFastestSector3Time = value;
+					NotifyPropertyChanged("mFastestSector3Time");
+				}
+			}
+		}
+		public float mFastestLapTime {
+			get { return _mFastestLapTime; }
+			set
+			{
+				if (_mFastestLapTime != value)
+				{
+					_mFastestLapTime = value;
+					NotifyPropertyChanged("mFastestLapTime");
+				}
+			}
+		}
+		public float mLastLapTime {
+			get { return _mLastLapTime; }
+			set
+			{
+				if (_mLastLapTime != value)
+				{
+					_mLastLapTime = value;
+					NotifyPropertyChanged("mLastLapTime");
+				}
+			}
+		}
+		public bool mLapInvalidated {
+			get { return _mLapInvalidated; }
+			set
+			{
+				if (_mLapInvalidated != value)
+				{
+					_mLapInvalidated = value;
+					NotifyPropertyChanged("mLapInvalidated");
+				}
+			}
+		}
+		public uint mRaceState {
+			get { return _mRaceState; }
+			set
+			{
+				if (_mRaceState != value)
+				{
+					_mRaceState = value;
+					NotifyPropertyChanged("mRaceState");
+				}
+			}
+		}
+		public uint mPitMode
+		{
+			get { return _mPitMode; }
+			set
+			{
+				if (_mPitMode != value)
+				{
+					_mPitMode = value;
+					NotifyPropertyChanged("mPitMode");
+				}
+			}
+		}
+		public float mSpeed { 
+			get { return _mSpeed; }
+			set
+			{
+				_mSpeed = value;
+				NotifyPropertyChanged("mSpeed");
+			}
+		}
+		public string cColor {
+			get { return _cColor; }
+			set
+			{
+				if (_cColor != value)
+				{
+					_cColor = value;
+					NotifyPropertyChanged("cColor");
+				}
+			}
+		}
+		public CTeam cTeam {
+			get { return _cTeam; }
+			set
+			{
+				if (_cTeam != value)
+				{
+					_cTeam = value;
+					NotifyPropertyChanged("cTeam");
+				}
+			}
+		}
 		public ParticipantInfo(uint i)
 		{
 			mWorldPosition = new ObservableCollection<float>();
@@ -78,6 +286,7 @@ namespace projectWpf.Sources.pages
 			mWorldPosition.Add(0);
 			mRacePosition = i;
 			cColor = "Red";
+			cTeam = new CTeam();
 		}
 	}
 	class MemObj : BaseViewModel
@@ -95,6 +304,8 @@ namespace projectWpf.Sources.pages
 		private int _mNumParticipants;
 		private ObservableCollection<ParticipantInfo> _mParticipantInfo;
 
+		private int _selectedRow;
+
 		public uint mVersion { get { return _mVersion; } set { _mVersion = value; NotifyPropertyChanged("mVersion"); } }
 		public uint mBuilderVersionNumber { get { return _mBuilderVersionNumber; } set { _mBuilderVersionNumber = value; NotifyPropertyChanged("mBuilderVersionNumber"); } }
 
@@ -108,17 +319,38 @@ namespace projectWpf.Sources.pages
 			get { return _mNumParticipants; }
 			set
 			{
-				for (int i = mNumParticipants; value > i; i++)
+				if (_mNumParticipants != value)
 				{
-					App.Current.Dispatcher.Invoke((Action)delegate
+					for (int i = mNumParticipants; value > i; i++)
 					{
-						mParticipantInfo.Add(new ParticipantInfo((uint)i + 1));
-					});
+						App.Current.Dispatcher.Invoke((Action)delegate
+						{
+							mParticipantInfo.Add(new ParticipantInfo((uint)i + 1));
+						});
+					}
+					for (int i = mNumParticipants; value < i; i++)
+					{
+						App.Current.Dispatcher.Invoke((Action)delegate
+						{
+							mParticipantInfo.RemoveAt(mParticipantInfo.Count - 1);
+						});
+					}
+					_mNumParticipants = value; NotifyPropertyChanged("mNumParticipants");
 				}
-				_mNumParticipants = value; NotifyPropertyChanged("mNumParticipants");
 			}
 		}
-		public ObservableCollection<ParticipantInfo> mParticipantInfo  { get { return _mParticipantInfo; } set { _mParticipantInfo = value; /*NotifyPropertyChanged("mParticipantInfo");*/  /*Debug.WriteLine($"patrinfo CHANGED");*/ } }
+		public ObservableCollection<ParticipantInfo> mParticipantInfo { get { return _mParticipantInfo; } set { _mParticipantInfo = value; /*NotifyPropertyChanged("mParticipantInfo");*/  /*Debug.WriteLine($"patrinfo CHANGED");*/ } }
+		public int selectedRow {
+			get { return _selectedRow; }
+			set
+			{
+				if (_selectedRow != value)
+				{
+					_selectedRow = value;
+					NotifyPropertyChanged("selectedRow");
+				}
+			}
+		}
 		public MemObj(int nParticipants = 0)
 		{
 			mParticipantInfo = new ObservableCollection<ParticipantInfo>();
---
+++ projectWpf/Sources/pages/cTeam.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace projectWpf.Sources.pages
+{
+	class CLap
+	{
+		private float _time;
+		public float time {
+			get { return _time; }
+			set
+			{
+				_time = value;
+			}
+		}
+		private float[] _sectors;
+		public float[] sectors {
+			get { return _sectors; }
+			set
+			{
+				_sectors = value;
+			}
+		}
+		private int _driver;
+		public int driver {
+			get { return _driver; }
+			set
+			{
+				_driver = value;
+			}
+		}
+		public CLap(int dri, float ti, float[] secs)
+		{
+			driver = dri;
+			time = ti;
+			sectors = new float[3];
+			sectors = secs;
+		}
+	}
+	class CTeam
+	{
+		private ObservableCollection<string> _drivers;
+		private int _curDriver;
+		private int _nextDriver;
+		public ObservableCollection<string> drivers {
+			get { return _drivers; }
+			set { _drivers = value; }
+		}
+		public ObservableCollection<CLap> Laps;
+		public int curDriver {
+			get { return _curDriver; }
+			set { _curDriver = value; }
+		}
+		public int nextDriver
+		{
+			get { return _nextDriver; }
+			set { _nextDriver = value; }
+		}
+		public CTeam()
+		{
+			Laps = new ObservableCollection<CLap>();
+			drivers = new ObservableCollection<string>();
+		}
+		public void AddDriver(string name)
+		{
+			drivers.Add(name);
+		}
+		public void AddLap(float time, float sec1, float sec2, float sec3)
+		{
+			Laps.Add(new CLap(curDriver, time, new float[] { sec1, sec2, sec3 }));
+		}
+	}
+}
